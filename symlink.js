306af31b6204236060427abebe0e3303d9c33f82
Oleg Postolovskyi <34275837+NewArtMask@users.noreply.github.com>
2023-08-30 10:57:58 +0300

    Update symlink.js

--- symlink.js
+++ symlink.js
@@ -1,8 +1,1 @@
-// Import the filesystem module
-const {symlink} = require('fs');
 
-// Create the symlink
-symlink("../../back-end/server", "./node_modules/server", 'file', (err) => {
-  if (err) console.log(err)
-  else console.log("done");
-});
